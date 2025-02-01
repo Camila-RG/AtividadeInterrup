89e71cdf251d51990a346a77e19a2a6be732d630
Camila-RG <camilaramosgomes@gmail.com>
2025-02-01 19:24:19 -0300

    Função para inicialização dos componentes criada

--- AtividadeInterrup.c
+++ AtividadeInterrup.c
@@ -1,12 +1,45 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
 
+//Definição dos pinos dos leds RGB, matriz de leds e botões
+#define LED_G_PIN 11
+#define LED_B_PIN 12
+#define LED_R_PIN 13
+
+#define BUTTON_A_PIN 5
+#define BUTTON_B_PIN 6
+
+#define MATRIZ_LEDS 7
+
+//Função para inicializar os leds e botões
+void inicializacao(){
+    
+    gpio_init(LED_G_PIN);
+    gpio_set_dir(LED_G_PIN, GPIO_OUT);
+    gpio_put(LED_G_PIN, 0);
+
+    gpio_init(LED_B_PIN);
+    gpio_set_dir(LED_B_PIN, GPIO_OUT);
+    gpio_put(LED_B_PIN, 0);
+
+    gpio_init(LED_R_PIN);
+    gpio_set_dir(LED_R_PIN, GPIO_OUT);
+    gpio_put(LED_R_PIN, 0);
+
+    gpio_init(BUTTON_A_PIN);
+    gpio_set_dir(BUTTON_A_PIN, GPIO_IN);
+    gpio_pull_up(BUTTON_A_PIN);
+
+    gpio_init(BUTTON_B_PIN);
+    gpio_set_dir(BUTTON_B_PIN, GPIO_IN);
+    gpio_pull_up(BUTTON_B_PIN);
+}
+
 int main()
 {
-    stdio_init_all();
+    while (true)
+    {
 
-    while (true) {
-        printf("Hello, world!\n");
-        sleep_ms(1000);
     }
+    return 0;
 }
