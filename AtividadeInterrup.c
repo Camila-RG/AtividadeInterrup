7953caa973bb6fe9afe91533317d7edebb45e832
Camila-RG <camilaramosgomes@gmail.com>
2025-02-01 20:02:10 -0300

    Função para o blink do led vermelho adicionada

--- AtividadeInterrup.c
+++ AtividadeInterrup.c
@@ -11,9 +11,11 @@
 
 #define MATRIZ_LEDS 7
 
+static volatile uint a = 0; //Contador
+
 //Função para inicializar os leds e botões
-void inicializacao(){
-    
+void inicializacaocomponentes(){
+
     gpio_init(LED_G_PIN);
     gpio_set_dir(LED_G_PIN, GPIO_OUT);
     gpio_put(LED_G_PIN, 0);
@@ -35,11 +37,37 @@ void inicializacao(){
     gpio_pull_up(BUTTON_B_PIN);
 }
 
+//Rotina da interrupção
+static void gpio_irq_handler(uint gpio, uint32_t events){
+    if (gpio_get(BUTTON_A_PIN)==0){
+
+    }    
+    else (gpio_get(BUTTON_B_PIN)==0);
+}
+
+//Função que fará o led vermelho picar 5 vezes por segundo
+void blink_red(){
+    for (int i = 0; i < 5; i++){
+        gpio_put(LED_R_PIN, 1);
+        sleep_ms(100);
+        gpio_put(LED_R_PIN, 0);
+        sleep_ms(100);
+    }
+}
+
+//Função principal
 int main()
 {
+    inicializacaocomponentes();
+    stdio_init_all();
+
+    //Configura a interrupção
+    gpio_set_irq_enabled_with_callback(BUTTON_A_PIN, GPIO_IRQ_EDGE_FALL, true, &gpio_irq_handler);
+
+    //Rotina principal
     while (true)
     {
-
+        blink_red();
     }
     return 0;
 }
